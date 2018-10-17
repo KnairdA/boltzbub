c67e4e1d68459fc29b6a1b4428256b6f86a43d78
Adrian Kummerlaender <knairda@t-online.de>
2018-10-17 12:48:37 +0200

    Fix density calculation in moving wall BC

--- src/boundary_conditions.cc
+++ src/boundary_conditions.cc
@@ -32,12 +32,7 @@ void computeWallCell(DataCellBuffer& pop, Vector<std::size_t> cell, Vector<int> 
 void computeMovingWallCell(DataCellBuffer& pop, Vector<std::size_t> cell, Vector<int> normal, Vector<double> u) {
 	const auto [neighborA, neighborB] = neighbors(normal);
 
-	const double rho = pop.curr(cell).get(-1,0) + pop.curr(cell).get(0,0) + pop.curr(cell).get(1,0)
-		+ 2.*(
-			pop.curr(cell).get(-neighborA) +
-			pop.curr(cell).get(-normal   ) +
-			pop.curr(cell).get(-neighborB)
-		);
+	const double rho = static_cast<Cell&>(pop.curr(cell)).sum();
 
 	pop.curr(cell).get(neighborA) = pop.curr(cell).get(-neighborA) - (6. * weight.get(-neighborA) * rho * (-neighborA * u));
 	pop.curr(cell).get(normal   ) = pop.curr(cell).get(-normal   ) - (6. * weight.get(-normal)    * rho * (-normal    * u));
